94cd6ca52b2975542ddbe63ad866e9c70142fc1c
Mark Lybrand <mlybrand@gmail.com>
2012-03-16 22:32:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fvtodo::Application.config.secret_token = '276492b88bd7c204563cf8dfd7ac5620bd925ead8f7d8d1f10fb5f5458b22f26d0a3fa6ad52682541cbdf84c6fbfd40a3d3b7a5e00bec54fef3f8ba59a6d8d98'
